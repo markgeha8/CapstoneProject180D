659859e6a2d8eb184b4dc10a930d5caa020195c6
Mark Geha <markgeha8@gmail.com>
2019-10-30 11:45:00 -0700

    Initial server code commit

---
+++ Server/serverConnect.py
@@ -0,0 +1,64 @@
+# Reminder: This is a comment. The first line imports a default library "socket" into Python.
+# You don’t install this. The second line is initialization to add TCP/IP protocol to the endpoint.
+import numpy as np
+import socket
+serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+iparray = np.zeros(40,dtype=str)
+
+#Test/Demo Purposes
+data = "14,10.15.32.15"
+
+# Assigns a port for the server that listens to clients connecting to this port.
+serv.bind(('0.0.0.0', 8080))
+serv.listen(40) #Assuming there will be 40 RPis attempting to connect (or something like that)
+while True:
+    conn, addr = serv.accept()
+    while True:
+        #data = conn.recv(4096)
+        #if not data: break
+        #data.decode()
+        print("Data provided is: ")
+        print(data)
+        print('\n')
+
+        recData = ""
+
+        #Empty the array if the received data is a RESET function
+        if(data == "RESET"):
+            del iparray[:]
+            iparray = np.zeros(40,dtype=str)
+            recData = "Reset"
+
+        #Parse the data into position and IP address.
+        else:
+            posBool = True
+            ipBool = True
+            pos = ""
+            ip = ""
+            i = 0
+            while posBool:
+                if(data[i] == ','):
+                    posBool = False
+                    break
+                pos = pos + data[i]
+                i = i+1
+            
+            while ipBool:
+                if(i > len(data)):
+                    ipBool = False
+                    break
+                ip = ip + data[i]
+                i = i+1
+
+            #Add the IP address to the array of IP addresses that will later be referenced
+            iparray[int(pos)-1] = ip
+
+            recData = pos
+
+        #conn.send((recData).encode())
+    #conn.close()
+    #print('Client Disconnected')
+    print("iparray[13]: ")
+    print(iparray[int(pos)-1])
+    print('\n')
