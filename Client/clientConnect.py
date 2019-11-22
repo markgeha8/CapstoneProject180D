243f44f517dd8dc15687b370a734720f9a860320
Mark Geha <markgeha8@gmail.com>
2019-11-21 19:06:44 -0800

    Server threads test

--- Client/clientConnect.py
+++ Client/clientConnect.py
@@ -49,6 +49,7 @@ while(not init_bool):
             break
 
 LED_displays = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+print("Exited Loop")
 
 #check that we have initialized the system
 
