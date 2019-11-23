fd5ac27700b186161eb9fbd12969dc664f3edb53
Mark Geha <markgeha8@gmail.com>
2019-11-23 15:28:14 -0800

    Updated serverThreads in 2D with clustering

--- Server/serverThreads2D.py
+++ Server/serverThreads2D.py
@@ -2,6 +2,8 @@
 #https://www.geeksforgeeks.org/socket-programming-python/
 
 import numpy as np
+from pylab import *
+from scipy.ndimage import measurements
 import socket
 from socket import AF_INET, SOCK_DGRAM
 import fcntl
@@ -12,9 +14,14 @@ serv = socket.socket(AF_INET, SOCK_DGRAM)
 serv.settimeout(10) #10 second delay per connection request
 
 maxStudents = 84
+maxRows = 20
+maxCols = 20
 maxTime = 1000
-ipArr = np.empty(maxStudents,dtype=object)
+ipArr = np.empty([maxRows,maxCols],dtype=object)
+numberOfClusters = 0
 done = False
+
+ipArrBin = np.zeros([maxRows,maxCols],dtype=int)
 
 #Gets IP address of server so others can connect (server's should be known by everyone beforehand)
 def get_ip_address(ifname):
@@ -31,8 +38,8 @@ def parseIP(data):
     return parsed
 
 #Updates the IP array with the correct position
-def updateIP(pos,ipAddress):
-    ipArr[pos-1] = ipAddress #Index
+def updateIP(posR,posC,ipAddress):
+    ipArr[posR-1,posC-1] = ipAddress #Index
     return
 
 #Sends message to the Client in byte form
@@ -40,10 +47,25 @@ def sendMess(message,ipAddress):
     serv.sendto(message,(ipAddress,8080))
     return
 
+def createBinaryArray(ipArr):
+    for r in range (0,maxRows):
+        for c in range (0,maxCols):
+            if(ipArr[r,c] == None):
+                ipArrBin[r,c] == 0
+            else:
+                ipArrBin[r,c] == 1
+    return ipArrBin
+
+def clusterData():
+    ipArrBin = createBinaryArray(ipArr)
+    clustered, numberOfClusters = measurements.label(ipArrBin)
+    return [clustered,numberOfClusters]
+
 #Thread 1: Focuses on connecting Clients and adding their IP addresses to the overall IP array
 def establishClientConnections():
     global done
     global ipArr
+    global numberOfClusters
 
     while True:
         try:
@@ -63,14 +85,15 @@ def establishClientConnections():
         
         else:
             if(data == "RESET"): #See if the whole IP array must be RESET because of a mistake
-                ipArr = np.empty(maxStudents,dtype=object)
+                ipArr = np.empty([maxRows,maxCols],dtype=object)
             
             else: #If all is good, parse the information, update the array, and confirm with that IP address
-                [pos,ipAddress] = parseIP(data)
-                position = int(pos)
-                updateIP(position,ipAddress)
+                [posR,posC,ipAddress] = parseIP(data)
+                posRow = int(posR)
+                posCol = int(posC)
+                updateIP(posRow,posCol,ipAddress)
 
-                mess = pos
+                mess = posR + ',' + posC
                 message = mess.encode()
                 sendMess(message,ipAddress)
             
@@ -79,28 +102,33 @@ def establishClientConnections():
 def propagateDisplayMessages():
     global done
     global ipArr
+    global numberOfClusters
 
     while True:
-        for add in range (0,len(ipArr)): #Move throughout the IP address loop
-            if(ipArr[add] != None): #"None" will define all the locations that are not connected
-                ipAddress = ipArr[add]
-                try: 
-                    mess = "runLED" #Sends them the code to start their LED run
-                    message = mess.encode()
-                    sendMess(message,ipAddress) #If they are not connected, this will be problematic and will cause the IP to be removed
-                except socket.timeout:
-                    ipArr[add] = None
-                    print("Timeout from IP address " + ipAddress)
-                    continue
-                    
-                waitTime = 0
+        [clusteredData,numberOfClusters] = clusterData()
+        for clustNum in range (0,numberOfClusters):
+            for posR in range (0,maxRows): #Move throughout the IP address loop
+                for posC in range (0,maxCols):
+                    if(ipArr[posR,posC] != None): #"None" will define all the locations that are not connected
+                        if(not(clusteredData[posR,posC] == clustNum)):
+                            ipAddress = ipArr[posR,posC]
+                            try: 
+                                mess = "runLED" #Sends them the code to start their LED run
+                                message = mess.encode()
+                                sendMess(message,ipAddress) #If they are not connected, this will be problematic and will cause the IP to be removed
+                            except socket.timeout:
+                                ipArr[posR,posC] = None
+                                print("Timeout from IP address " + ipAddress)
+                                continue
+                                
+                            waitTime = 0
 
-                while(not done):
-                    if(waitTime >= maxTime): #If there is no response for longer than maxTime iterations, it will be removed (failsafe)
-                        ipArr[add] = None
-                        break
-                    waitTime = waitTime+1
-                done = False
+                            while(not done):
+                                if(waitTime >= maxTime): #If there is no response for longer than maxTime iterations, it will be removed (failsafe)
+                                    ipArr[posR,posC] = None
+                                    break
+                                waitTime = waitTime+1
+                            done = False
 
 # Main function
 if __name__ == "__main__":
