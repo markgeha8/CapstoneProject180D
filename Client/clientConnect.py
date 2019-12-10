dee510c826c5463e69982e213335effff39c553d
Mark Geha <markgeha8@gmail.com>
2019-12-10 07:45:07 -0800

    Update IP address

--- Client/clientConnect.py
+++ Client/clientConnect.py
@@ -17,7 +17,7 @@ connect = True
 connected = False
 
 ipToken = '172.20.10.11'
-ipServer = '172.20.10.20'
+ipServer = '172.20.10.5'
 
 #Parse the position and IP address String. Returns a list.
 def parseData(data):
