03bb800462cbed1878ff5abba91b296df3df57a9
Bennett Juwvipart <benjuw@gmail.com>
2020-01-30 12:01:47 -0800

    Update accel_test.py

--- Undercooked/Gesture/accel_test.py
+++ Undercooked/Gesture/accel_test.py
@@ -18,7 +18,7 @@ while truth == True:
     ACCy = IMU.readACCy()*.244/1000
     ACCz = IMU.readACCz()*.244/1000
     print("X = "+str(ACCx)+"G     "+"Y = "+str(ACCy)+"G     "+"Z = "+str(ACCz)+"G     ")
-    #print("Time is: " +str(time.time() - start))
+    print("Time is: " +str(time.time() - start))
 
 
     #initial detection
@@ -36,9 +36,24 @@ while truth == True:
     if (cut == True) and (time.time()-start < 7):
         if ACCy>5:
             cut_num = cut_num+1
-            #print("cutting motion detected" + str(cut_num) + "/10")
+            print("cutting motion detected" + str(cut_num) + "/10")
         if cut_num == 10:
-            #print("ingredient cut")
+            print("ingredient cut")
+            truth = False
+            start = time.time()
+            cut_num = 0
+            cut = False
+            cook_num = 0
+            cook = False
+            md = False
+
+    #Cooking - must cook 5 motions to be done
+    if (cook == True) and (time.time()-start < 7):
+        if ACCx>5:
+            cook_num = cook_num+1
+            print("cooking motion detected" + str(cook_num) + "/10")
+        if cook_num == 10:
+            print("ingredient cooked")
             truth = False
             start = time.time()
             cut_num = 0
