bb1ad91ec4f41deeaea652cdb6fd8e2e885efb51
Bennett Juwvipart <benjuw@gmail.com>
2020-01-30 11:49:04 -0800

    Update accel_test.py

--- Undercooked/Gesture/accel_test.py
+++ Undercooked/Gesture/accel_test.py
@@ -17,7 +17,7 @@ while truth == True:
     ACCx = IMU.readACCx()*.244/1000
     ACCy = IMU.readACCy()*.244/1000
     ACCz = IMU.readACCz()*.244/1000
-    print("X = "+str(ACCx)+"G     "+"Y = "+str(ACCy)+"G     "+"Z = "+str(ACCz)+"G     ")
+    #print("X = "+str(ACCx)+"G     "+"Y = "+str(ACCy)+"G     "+"Z = "+str(ACCz)+"G     ")
     print("Time is: " +str(time.time() - start))
 
 
@@ -35,8 +35,8 @@ while truth == True:
     #Cutting - must cut 5 motions to be done
     if (cut == True) and (time.time()-start > 7):
         if ACCy>3:
-            print("cutting motion detected")
             cut_num = cut_num+1
+            print("cutting motion detected" + str(cut_num) + "/10")
         if cut_num == 10:
             print("ingredient cut")
             truth = False
