2d029e3f488c058146823271dcc0e4301b568fc3
Bennett Juwvipart <benjuw@gmail.com>
2020-01-30 10:47:01 -0800

    Update accel_test.py

--- Undercooked/Gesture/accel_test.py
+++ Undercooked/Gesture/accel_test.py
@@ -7,7 +7,7 @@ IMU.initIMU()       #Initialise the accelerometer, gyroscope and compass
 
 truth = True
 
-while truth = True:
+while truth == True:
 
 
 	#Read the accelerometer,gyroscope and magnetometer values
@@ -15,13 +15,15 @@ while truth = True:
 	ACCy = IMU.readACCy()* .244/1000
 	ACCz = IMU.readACCz()* .244/1000
 
+    print("##### X = %f G  #####" % (ACCx),
+	print(" Y =   %fG  #####" % (ACCy),
+	print(" Z =  %fG  #####" % (ACCz)
+
     if(ACCx > 2 | ACCy > 2 | ACCz >2)
         print("motion detected")
         truth = False
 
-	print("##### X = %f G  #####" % (ACCx),
-	print(" Y =   %fG  #####" % (ACCy),
-	print(" Z =  %fG  #####" % (ACCz)
+
 
 
 
