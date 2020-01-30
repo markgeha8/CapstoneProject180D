9d007a43593a95320d26739ce12c8c850d92e04b
Bennett Juwvipart <benjuw@gmail.com>
2020-01-30 11:18:52 -0800

    Update accel_test.py

--- Undercooked/Gesture/accel_test.py
+++ Undercooked/Gesture/accel_test.py
@@ -8,7 +8,7 @@ while truth == True:
     ACCy = IMU.readACCy()*.244/1000
     ACCz = IMU.readACCz()*.244/1000
     print("X = "+str(ACCx)+"G     "+"Y = "+str(ACCy)+"G     "+"Z = "+str(ACCz)+"G     ")
-    if (Accx>2):
+    if (ACCx>2) or (ACCy>2) or (ACCz>2):
         print("motion detected")
         truth = False
     time.sleep(0.03)
