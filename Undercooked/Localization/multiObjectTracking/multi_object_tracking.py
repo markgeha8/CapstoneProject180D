21331194d363f048072b0263cbe9133e4b2844f5
Mark Geha <markgeha8@gmail.com>
2020-02-06 14:43:10 -0800

    Updated tracking

--- Undercooked/Localization/multiObjectTracking/multi_object_tracking.py
+++ Undercooked/Localization/multiObjectTracking/multi_object_tracking.py
@@ -51,7 +51,7 @@ args = vars(ap.parse_args())
 # initialize a dictionary that maps strings to their corresponding
 # OpenCV object tracker implementations
 OPENCV_OBJECT_TRACKERS = {
-	"csrt": cv2.TrackerCSRT_create,
+	#"csrt": cv2.TrackerCSRT_create,
 	"kcf": cv2.TrackerKCF_create,
 	"boosting": cv2.TrackerBoosting_create,
 	"mil": cv2.TrackerMIL_create,
