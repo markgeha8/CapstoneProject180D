088ea5dbc91982f508b1628f1d72fd2a9aa13215
Bennett Juwvipart <benjuw@gmail.com>
2020-02-07 18:07:39 -0800

    Create send_data.py

---
+++ Undercooked/Gesture/send_data.py
@@ -0,0 +1,15 @@
+import time
+import IMU
+import socket
+from socket import AF_INET, SOCK_DGRAM
+import threading
+
+
+
+send_data = "chop"
+
+
+
+while True:
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client.sendto(send_data.encode(), ('192.168.1.182',8080))
