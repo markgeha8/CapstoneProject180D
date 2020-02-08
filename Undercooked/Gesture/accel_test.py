5fa22143ee6ea6ff0db1ea3b8fdcd5fb810c5023
Bennett Juwvipart <benjuw@gmail.com>
2020-02-07 17:34:40 -0800

    Update accel_test.py

--- Undercooked/Gesture/accel_test.py
+++ Undercooked/Gesture/accel_test.py
@@ -27,7 +27,7 @@ while truth == True:
     print("Time is: " +str(time.time() - start))
 
 
-    if ((ACCx>6) or (ACCy>6)):
+    if ((ACCx>4.5) or (ACCy>4.5)):
         if ACCy>ACCx:
             print("cutting motion detected")
             cut_num = cut_num+1
