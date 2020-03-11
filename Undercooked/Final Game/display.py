60e7b09912ced2b80787c603362e048fd45013a3
Charlotte McGinn <charlotte.mcginn@engineering.ucla.edu>
2020-03-10 20:56:34 -0700

    display

--- Undercooked/Final Game/display.py
+++ Undercooked/Final Game/display.py
@@ -34,11 +34,19 @@ line3.pack()
 line4 = Label(window, textvariable=line4txt, font=("Arial", 20), justify=LEFT)
 line4.pack()
 
+line5 = Label(window, text="________________________________", font=("Arial", 20), justify=LEFT)
+line5.pack()
+
+line6 = Label(window, text="Items cooked on stove: chicken, rice, seaweed", font=("Arial", 20), justify=LEFT)
+line6.pack()
+
+line7 = Label(window, text="Items chopped on cuttingboard: fish, tomato, lettuce", font=("Arial", 20), justify=LEFT)
+line7.pack()
+
 while True:
-    sleep(1) # Need this to slow the changes down
     line1txt.set("Current Recipe: " + line1)
     line2txt.set("Ingredients: " + line2)
     line3txt.set("Plate: " + line3)
     line4txt.set("Score: " + line4)
 
-    root.update_idletasks()
+    window.update()
