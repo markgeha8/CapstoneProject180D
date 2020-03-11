cc36adb85b9f87a8beb64f6149b3af240bfeffdc
Charlotte McGinn <charlotte.mcginn@engineering.ucla.edu>
2020-03-10 20:59:13 -0700

    display

--- Undercooked/Final Game/display.py
+++ Undercooked/Final Game/display.py
@@ -5,10 +5,10 @@ window = Tk()
 window.title("Undercooked")
 window.geometry('650x500')
 
-line1 = "1"
-line2 = "2"
-line3 = "3"
-line4 = "4"
+line1Str = "1"
+line2Str = "2"
+line3Str = "3"
+line4Str = "4"
 
 line1txt = StringVar()
 line1txt.set("Current Recipe: ")
@@ -37,16 +37,25 @@ line4.pack()
 line5 = Label(window, text="________________________________", font=("Arial", 20), justify=LEFT)
 line5.pack()
 
-line6 = Label(window, text="Items cooked on stove: chicken, rice, seaweed", font=("Arial", 20), justify=LEFT)
+line6 = Label(window, text="Items cooked on stove:", font=("Arial", 20), justify=LEFT)
 line6.pack()
 
-line7 = Label(window, text="Items chopped on cuttingboard: fish, tomato, lettuce", font=("Arial", 20), justify=LEFT)
+line7 = Label(window, text="Chicken, Rice, Seaweed", font=("Arial", 20), justify=LEFT)
 line7.pack()
 
+line8 = Label(window, text="________________________________", font=("Arial", 20), justify=LEFT)
+line8.pack()
+
+line9 = Label(window, text="Items chopped on cuttingboard:", font=("Arial", 20), justify=LEFT)
+line9.pack()
+
+line10 = Label(window, text="Fish, Tomato, Lettuce", font=("Arial", 20), justify=LEFT)
+line10.pack()
+
 while True:
-    line1txt.set("Current Recipe: " + line1)
-    line2txt.set("Ingredients: " + line2)
-    line3txt.set("Plate: " + line3)
-    line4txt.set("Score: " + line4)
+    line1txt.set("Current Recipe: " + line1Str)
+    line2txt.set("Ingredients: " + line2Str)
+    line3txt.set("Plate: " + line3Str)
+    line4txt.set("Score: " + line4Str)
 
     window.update()
