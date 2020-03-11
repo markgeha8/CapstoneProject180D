3436291c65082cadea7d584b4ccd0259c84f7fbd
Charlotte McGinn <charlotte.mcginn@engineering.ucla.edu>
2020-03-10 20:32:31 -0700

    display

--- Undercooked/Final Game/display.py
+++ Undercooked/Final Game/display.py
@@ -5,16 +5,40 @@ window = Tk()
 window.title("Undercooked")
 window.geometry('650x500')
 
-line1 = Label(window, text="Current Recipe: Sushi", font=("Arial", 20), justify=LEFT)
-line1.grid(column=0, row=0)
+line1 = "1"
+line2 = "2"
+line3 = "3"
+line4 = "4"
 
-line2 = Label(window, text="Ingredients: Fish, Rice, Seaweed", font=("Arial", 20), justify=LEFT)
-line2.grid(column=0, row=1)
+line1txt = StringVar()
+line1txt.set("Current Recipe: ")
 
-line3 = Label(window, text="Plate: Fish", font=("Arial", 20), justify=LEFT)
-line3.grid(column=0, row=2)
+line2txt = StringVar()
+line2txt.set("Ingredients: ")
 
-line4 = Label(window, text="Score: 10", font=("Arial", 20), justify=LEFT)
-line4.grid(column=0, row=3)
+line3txt = StringVar()
+line3txt.set("Plate: ")
 
-window.mainloop()
+line4txt = StringVar()
+line4txt.set("Score: ")
+
+line1 = Label(window, textvariable=line1txt, font=("Arial", 20), justify=LEFT)
+line1.pack()
+
+line2 = Label(window, textvariable=line2txt, font=("Arial", 20), justify=LEFT)
+line2.pack()
+
+line3 = Label(window, textvariable=line3txt, font=("Arial", 20), justify=LEFT)
+line3.pack()
+
+line4 = Label(window, textvariable=line4txt, font=("Arial", 20), justify=LEFT)
+line4.pack()
+
+while True:
+    sleep(1) # Need this to slow the changes down
+    line1txt.set("Current Recipe: " + line1)
+    line2txt.set("Ingredients: " + line2)
+    line3txt.set("Plate: " + line3)
+    line4txt.set("Score: " + line4)
+
+    root.update_idletasks()
