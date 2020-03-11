9fd36f6cb5a66f3867e0276a372d620cbf2b9ffe
Mark Geha <markgeha8@gmail.com>
2020-03-10 21:49:05 -0700

    UPdate

--- Undercooked/Final Game/game-logic-two-player.py
+++ Undercooked/Final Game/game-logic-two-player.py
@@ -2,7 +2,7 @@ import socket
 import threading
 import numpy as np
 from enum import Enum
-from gameenums import Gesture, Location, VoiceCommand, MenuItem, Ingredient, IngredientStatus
+from gameenums import *
 import localization 
 import colorDetect
 import voiceRecog
@@ -68,6 +68,7 @@ menu_to_recipe = {
 location_to_current_ingredient = {
     Location.CUTTINGBOARD: None,
     Location.STOVE: None,
+    Location.NONE: None,
 }
 
 ingredient_to_valid_location = {
@@ -397,12 +398,12 @@ def setupDisplay():
     
     currentRecipeString = ""
     for i in range(len(currentRecipe)): 
-        currentRecipeString += ingredient_to_name[currentRecipe[i].name] + " "
+        currentRecipeString += ingredient_enum_to_name[currentRecipe[i].name] + " "
 
     window.title("Undercooked")
     window.geometry('650x500')
 
-    line1txt.set("Order: " + menuItem_to_name[currentOrder])
+    line1txt.set("Order: " + menuItem_enum_to_name[currentOrder])
 
     line2txt.set("Recipe: " + currentRecipeString)
 
@@ -451,13 +452,13 @@ def updateDisplay():
 
     currentRecipeString = ""
     for i in range(len(currentRecipe)): 
-        currentRecipeString += ingredient_to_name[currentRecipe[i].name] + " "
+        currentRecipeString += ingredient_enum_to_name[currentRecipe[i].name] + " "
 
     currentPlateString = ""
     for i in range(len(currentPlate)): 
         currentPlateString += ingredient_to_name[currentPlate[i].name] + " "
 
-    line1txt.set("Order: " + menuItem_to_name[currentOrder])
+    line1txt.set("Order: " + menuItem_enum_to_name[currentOrder])
     line2txt.set("Recipe: " + currentRecipeString)
     line3txt.set("Plate: " + currentPlateString)
     line4txt.set("Score: " + str(points))
