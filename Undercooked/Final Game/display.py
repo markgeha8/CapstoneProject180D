31ad3d76f9036872ca699bd04bd9eee0c31896f7
Charlotte McGinn <charlotte.mcginn@engineering.ucla.edu>
2020-03-10 20:10:51 -0700

    display

--- Undercooked/Final Game/display.py
+++ Undercooked/Final Game/display.py
@@ -5,16 +5,16 @@ window = Tk()
 window.title("Undercooked")
 window.geometry('650x500')
 
-line1 = Label(window, text="Current Recipe: Sushi", font=("Arial", 20))
+line1 = Label(window, text="Current Recipe: Sushi", font=("Arial", 20), justify=LEFT)
 line1.grid(column=0, row=0)
 
-line2 = Label(window, text="Ingredients: Fish, Rice, Seaweed", font=("Arial", 20))
+line2 = Label(window, text="Ingredients: Fish, Rice, Seaweed", font=("Arial", 20), justify=LEFT)
 line2.grid(column=0, row=1)
 
-line3 = Label(window, text="Plate: Fish", font=("Arial", 20))
-line2.grid(column=0, row=2)
+line3 = Label(window, text="Plate: Fish", font=("Arial", 20), justify=LEFT)
+line3.grid(column=0, row=2)
 
-line4 = Label(window, text="Score: 10", font=("Arial", 20))
+line4 = Label(window, text="Score: 10", font=("Arial", 20), justify=LEFT)
 line4.grid(column=0, row=3)
 
 window.mainloop()
